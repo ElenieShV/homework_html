4049577cf35c38102b9c1cb8bc06b04791b91fb0
ElenieShV <dr.elena.shved@gmail.com>
2025-07-04 23:58:32 +0300

    добавила папку cats и котиков для галереи

---
+++ index.py
@@ -0,0 +1,152 @@
+
+'''
+amount=int(input("Enter product's amount:"))
+if amount>=5:
+   print("20% discount") 
+elif amount==4 or amount==3:
+   print("10% discount") 
+else: 
+    print("Without discount")   
+'''    
+
+
+'''
+score=int(input("Enter score"))
+elseif score>=90:
+   print("A")
+elif score>=75 and score<=89:
+    print("B")
+elif score>=60 and score<=74:
+    print("C")   
+elif score<60:
+    print("F")
+else:
+    print("Mark doesn't exist")    
+'''   
+'''
+time=float(input("Enter time"))
+if time>=6 and time<=11:
+    print("Good morning!")
+elif time>=12 and time<=17:
+    print("Good afternoon!")   
+elif time>=18 and time<=21:
+    print("Good evening!")  
+else:      
+      print("Good night!")
+
+
+
+celsius=float(input("Enter temperature in celsius:"))
+farenheit=celsius*9/5+32
+print(f"Temperature in farenheit:{farenheit}")'''
+
+'''euros=float(input("Enter the amount in euros:"))
+rate=1.3
+dollars=euros*rate
+print(f"Amount in dollars: {dollars}")
+'''
+
+'''number=int(input("Enter number:"))
+print(number//10)
+print(number%10)
+
+# digit1=input("Enter the first digit:")
+# digit2=input("Enter the second digit")
+# result=int(digit1+digit2)
+# print(f"The number is: {result}")
+
+# number=int(input("Enter a three-digit number:"))
+# digit1=number//100
+# digit2=number%100//10
+# digit3=number%10
+# print(digit1)
+# print(digit2)
+# print(digit3)
+# sum_digits=digit1+digit2+digit3
+
+# 
+
+# steps=int(input("Enter the number os steps:"))
+# if steps<5000:
+#     print("Today you moved too little")
+# elif steps<=10000:
+#     print("Not bad")  
+# else:
+#     print("Well done")    
+    
+# 
+
+# month = int(input("Enter the number of the month (1-12): "))
+
+# if month in [12, 1, 2]:
+#     print("Winter")
+# elif month in [3, 4, 5]:
+#     print("Spring")
+# elif month in [6, 7, 8]:
+#     print("Summer")
+# elif month in [9, 10, 11]:
+#     print("Autumn")
+# else:
+#     print("Invalid month number")
+
+
+
+# num=int(input("Enter num:"))
+# if num!=0:
+#    if num%2==0:
+#       print("Even num")
+#    else:
+#        print("Odd num")
+# else:
+#     print("This is 0!")          
+
+
+# age=int(input("Enter your age:"))
+# if age>=18:
+#     print("Access granted")
+# else:
+#     print("Access denied")    
+
+
+# num1=int(input("Enter 1st number:"))
+# num2=int(input("Enter 2nd number:"))
+# if num1>num2:
+#    max=num1
+# else:
+#     max=num2
+# print("max:", max)       
+
+
+# password=input("Enter your password:")
+# if len(password)>=8:
+#     print("Password is srtong")
+# else:
+#     print("Password is too short") 
+    
+    
+    
+# a=int(input("Enter side A:"))
+# b=int(input("Enter side B:"))
+# c=int(input("Enter side C:"))
+# if a+b>c:
+#     print("Triangle is not possible")
+# elif a+c<=b:
+#     print("Triangle is not possible")
+# elif b+c<=a:
+#     print("Triangle is not possible") 
+# else:
+#     print("Triangle is possible")                  
+
+
+'''
+# happiness=str("full_satisfaction")
+# if happiness!=str("full_satisfaction"):
+#     print("You should change something!")
+# elif happiness==str("full_satisfaction"):
+#     print("Great job, girl! Enjoy!")
+# else:
+#     print("Impossible")    
+#     '''
+
+
+
